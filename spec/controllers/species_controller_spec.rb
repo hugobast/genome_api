4f2f784baf44824cab0b65c5ea56cef41a4d6593
Hugo Bastien <hugobast@gmail.com>
2014-03-30 16:47:47 -0400

    Giving the tests better descriptions

--- spec/controllers/species_controller_spec.rb
+++ spec/controllers/species_controller_spec.rb
@@ -8,14 +8,14 @@ describe SpeciesController do
   end
 
   describe "GET 'index'" do
-    it "returns http success for json response" do
+    it "returns all species json response" do
       get :index, format: :json
       expect(response.body).to match("E. coli")
     end
   end
 
   describe "GET 'show'" do
-    it "returns http success for json response" do
+    it "returns all species gene sequences json response" do
       get :show, id: 1, format: :json
       expect(response.body).to match("gidA")
       expect(response.body).to match("dnaA")
