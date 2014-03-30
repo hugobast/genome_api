78bad834030fc1acf530a290af5b82d9c14aac97
Hugo Bastien <hugobast@gmail.com>
2014-03-30 15:59:35 -0400

    Extraction of logic of the task so we can reuse in seed

---
+++ lib/genome_import_task.rb
@@ -0,0 +1,45 @@
+require "open-uri"
+require "dna/genome"
+
+class GenomeImportTask
+  attr_reader :dna
+
+  def initialize(path)
+    @path = path
+  end
+
+  def call
+    open(@path) do |file|
+      @dna = DNA::Genome.new(file)
+
+      species = species_with_name(dna.species)
+      create_sequences(species)
+    end
+  end
+
+  private
+
+  def create_sequences(species)
+    dna.each_coding_sequences do |coding_sequence|
+      create_sequence(coding_sequence, dna, species)
+    end
+  end
+
+  def create_sequence(coding_sequence, dna, species)
+    sequence = sequence_with_protein(coding_sequence.protein_id)
+
+    sequence.update_attributes(
+      species: species,
+      translation: coding_sequence.translation,
+      dna: dna.sequence_range(coding_sequence.range)
+    )
+  end
+
+  def sequence_with_protein(protein_id)
+    Sequence.find_or_create_by_gene(protein_id)
+  end
+
+  def species_with_name(name)
+    Species.find_or_create_by_name(name)
+  end
+end
